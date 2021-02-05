cdf9b57acc6b81f6dec5617bef4cc6fe46defe61
Manuel <carlosd99m@gmail.com>
2021-02-05 01:07:06 -0600

    Se agrego el Modelo, Factory, Controler, migración

---
+++ database/factories/EmpleadoFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Empleado;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmpleadoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Empleado::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'lastName' => $this->faker->text(),
+            'phone' => $this->faker->numberBetween(1,14),
+            'dni' => $this->faker->dni,
+            'direction' => $this->faker->paragraph(),
+
+        ];
+    }
+}
---
+++ database/migrations/2021_02_05_065338_create_empleados_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('lastName', 50);
+            $table->integer('phone', 20);
+            $table->string('dni', 20);
+            $table->text('direction', 150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
